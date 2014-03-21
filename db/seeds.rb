b3b8b4709e060fc9b43bf651043b406c11aaef4c
Shelby Doolittle <shelby.doolittle@agilquest.com>
2014-03-21 09:43:25 -0400

    fix(seed): seed with valid user data
    Previous user data for seeding was invalid.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = User.where(:username => 'benhamin_harrison').first_or_create
+user = User.where(:username => 'benjamin_harrison').first_or_initialize
 user.password = 'password'
+user.password_confirmation = 'password'
 
 role = Role.where(:name => 'charity registrar').first_or_create
 
