8da70c507623cb11eb1bd5097664ad22158e7e12
Shelby Doolittle <shelby@shelbyd.com>
2014-03-29 16:14:35 -0400

    fix(analytics): default SEGMENTIO_WRITE_KEY to ''
    Default the secret for Analytics to an empty string.

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,6 +1,6 @@
 # Segment.io analytics
 Analytics = AnalyticsRuby
 Analytics.init({
-    secret: ENV["SEGMENTIO_WRITE_KEY"],
+    secret: ENV["SEGMENTIO_WRITE_KEY"] || '',
     on_error: Proc.new { |status, msg| print msg }
 })
