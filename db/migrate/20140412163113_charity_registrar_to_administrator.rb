14d788f8d1b55e3b2d2ed9aedfce55c61436c273
Shelby Doolittle <shelby@shelbyd.com>
2014-04-12 12:43:40 -0400

    fix(migration): allow migration to not fail if old role does not exist
    Migration was failing during tests because there were no roles at all in the database.

--- db/migrate/20140412163113_charity_registrar_to_administrator.rb
+++ db/migrate/20140412163113_charity_registrar_to_administrator.rb
@@ -1,7 +1,9 @@
 class CharityRegistrarToAdministrator < ActiveRecord::Migration
   def change
     role = Role.find_by_name('charity registrar')
-    role.name = Constants::Roles::CHARITY_ADMINISTRATOR
-    role.save!
+    unless role.nil?
+      role.name = Constants::Roles::CHARITY_ADMINISTRATOR
+      role.save!
+    end
   end
 end
